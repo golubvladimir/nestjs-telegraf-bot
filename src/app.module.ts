5618d170badfeee9b7b9eae5f69a9791a6ef990b
Vladimir Golub <golubvlauri@gmail.com>
2022-11-19 23:23:43 +0200

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TelegrafModule } from 'nestjs-telegraf';
 import { EchoModule } from './echo/echo.module';
+import { EchoUpdate } from './echo/echo.update';
 
 @Module({
   imports: [
     TelegrafModule.forRoot({
-      token: '***',
-      include: [
-        EchoModule
-      ]
-    })
+      token: '+++',
+    }),
+    EchoModule
   ],
 })
 export class AppModule {}
--- src/echo/echo.update.ts
+++ src/echo/echo.update.ts
@@ -1,7 +1,9 @@
 import { Update, Start, Help } from 'nestjs-telegraf';
 import { Context } from 'telegraf';
+import { Injectable } from '@nestjs/common';
 
 @Update()
+@Injectable()
 export class EchoUpdate {
 
   @Start()
